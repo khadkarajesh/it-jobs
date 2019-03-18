76aa9a7241c91cdd30fd142a8096e05fca351aa5
Rajesh <rajesh.k.khadka@gmail.com>
2019-03-18 12:32:28 +0545

    add bottom navigation

--- app/src/main/java/com/softwarejobs/job/HomeActivity.kt
+++ app/src/main/java/com/softwarejobs/job/HomeActivity.kt
@@ -4,11 +4,29 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addFragment(HomeFragment.getInstance())
+        home_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.menu.home_menu -> {
+                    addFragment(HomeFragment.getInstance())
+                }
+            }
+            true
+        }
+
+    }
+
+    private fun addFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
     }
 
     companion object {
