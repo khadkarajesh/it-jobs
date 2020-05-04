c3ad5d6bc928a9a84aa960255697216b10cb796f
rajesh <rajesh.k.khadka@gmail.com>
2020-05-04 22:46:23 +0545

    add login

---
+++ lib/src/login/bloc/login_bloc.dart
@@ -0,0 +1,79 @@
+import 'dart:async';
+import 'package:bloc/bloc.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:jobs/src/data/api/user_repository.dart';
+import 'package:jobs/src/login/bloc/login_state.dart';
+import 'package:jobs/src/utils/validators.dart';
+import 'package:rxdart/rxdart.dart';
+import './bloc.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  UserRepository _userRepository;
+
+  LoginBloc({@required UserRepository userRepository})
+      : assert(userRepository != null),
+        _userRepository = userRepository;
+
+  @override
+  LoginState get initialState => LoginState.empty();
+
+  @override
+  Stream<Transition<LoginEvent, LoginState>> transformEvents(
+      Stream<LoginEvent> events, transitionFn) {
+    final nonDebounceStream = events.where((event) {
+      return (event is! EmailChanged && event is! PasswordChanged);
+    });
+
+    final debounceStream = events.where((event) {
+      return (event is EmailChanged || event is PasswordChanged);
+    }).debounceTime(Duration(milliseconds: 300));
+
+    return super.transformEvents(
+        nonDebounceStream.mergeWith([debounceStream]), transitionFn);
+  }
+
+  @override
+  Stream<LoginState> mapEventToState(
+    LoginEvent event,
+  ) async* {
+    if (event is EmailChanged) {
+      yield* _mapEmailChangedToState(event.email);
+    } else if (event is PasswordChanged) {
+      yield* _mapPasswordChangedToState(event.password);
+    } else if (event is LoginWithCredentialsPressed) {
+      yield* _mapLoginWithCredentialsPressedToState();
+    } else if (event is LoginWithGooglePressed) {
+      yield* _mapLoginWithGooglePressedState();
+    }
+  }
+
+  Stream<LoginState> _mapEmailChangedToState(String email) async* {
+    yield state.update(
+      isEmailValid: Validators.isValidEmail(email),
+    );
+  }
+
+  Stream<LoginState> _mapPasswordChangedToState(String password) async* {
+    yield state.update(isPasswordValid: Validators.isValidPassword(password));
+  }
+
+  Stream<LoginState> _mapLoginWithGooglePressedState() async* {
+    try {
+      await _userRepository.signInWIthGoogle();
+      yield LoginState.success();
+    } catch (_) {
+      yield LoginState.failure();
+    }
+  }
+
+  Stream<LoginState> _mapLoginWithCredentialsPressedToState(
+      {String email, String password}) async* {
+    yield LoginState.loading();
+    try {
+      await _userRepository.signInWithCredentials(email, password);
+      yield LoginState.success();
+    } catch (_) {
+      yield LoginState.failure();
+    }
+  }
+}
---
+++ lib/src/widgets/login_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:jobs/src/data/api/user_repository.dart';
+import 'package:jobs/src/login/bloc/login_bloc.dart';
+import 'package:jobs/src/widgets/login_form.dart';
+import 'package:nepninja/nepninja.dart';
+
+class LoginScreen extends StatelessWidget {
+
+  final UserRepository _userRepository;
+
+  LoginScreen({Key key, @required UserRepository userRepository})
+      : assert(userRepository != null),
+        _userRepository = userRepository,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: BlocProvider<LoginBloc>(
+          create: (context) => LoginBloc(userRepository:_userRepository),
+          child: LoginForm(userRepository: _userRepository,),
+        ));
+  }
+}
